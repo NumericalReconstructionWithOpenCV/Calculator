383404a940769cc63b9fc06a32361efd688e92f0
stories2 <stories2@naver.com>
2017-03-29 03:59:59 +0900

    FileIO Module : input image file
    이미지 파일을 로드하는 기능을 테스트하고 성공함

---
+++ FileIO.py
@@ -0,0 +1,6 @@
+import cv2
+import unittest2 as unittest
+from matplotlib import pyplot as plt
+
+def LoadImage(pathOfImage):
+    return plt.imread(pathOfImage)
--- ProgramMain.py
+++ ProgramMain.py
@@ -1,5 +1,7 @@
 import cv2
 import unittest2 as unittest
+import FileIO as fileIO
+from matplotlib import pyplot as plt
 
 print "Hello World, We will check your project Setting"
 
@@ -8,6 +10,12 @@ class ProjectSettingTest(unittest.TestCase):
     def TestVersionOfOpenCV(self):
         self.assertEqual(cv2.__version__, "3.2.0", "Wrong Version Installed")
 
+class ProjectCalculateLength(unittest.TestCase):
+
+    def ImageLoadTest(self):
+        self.assertNotEqual(fileIO.LoadImage("Resources/beforeImage.png"), None)
+
 testSuite = unittest.TestSuite()
 testSuite.addTest(ProjectSettingTest('TestVersionOfOpenCV'))
+testSuite.addTest(ProjectCalculateLength('ImageLoadTest'))
 unittest.TextTestRunner(verbosity=2).run(testSuite)
