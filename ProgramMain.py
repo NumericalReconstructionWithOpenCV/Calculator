dd08c636d2bb0136bca3ca5be21741186fee652f
Kim HyunWoo <stories2@gimhyeon-uui-MacBook-Pro.local>
2017-04-17 08:49:17 +0900

    Update Project Settings
    Change Project Structure

--- ProgramMain.py
+++ ProgramMain.py
@@ -1,47 +1,17 @@
 import cv2
 import unittest
-import FileIO as fileIO
 import ObjectDetect as objectDetect
 import FindCorner
-from matplotlib import pyplot as plt
-import numpy as np
+import Setting.DefineManager
 
 class ProjectSettingTest(unittest.TestCase):
 
     def TestVersionOfOpenCV(self):
-        self.assertEqual(cv2.__version__, "3.2.0", "Wrong Version Installed")
-
-class ProjectCalculateLength(unittest.TestCase):
-    def TestingDetecting(self):
-        objectDetect.Detecting()
-
-    def MakeBlankImage(self):
-        global blankImage
-        blankImage = np.zeros((1280, 720, 3), np.uint8)
-
-    def BeforeImageLoadTest(self):
-        global beforeImage, blankImage
-        beforeImage = "Resources/testcase4/before.png"
-        self.assertNotEqual(beforeImage, blankImage)
-
-    def AfterImageLoadTest(self):
-        global afterImage, blankImage
-        afterImage = "Resources/testcase4/after.png"
-        self.assertNotEqual(afterImage, blankImage)
-
-    def ObjectDetectFromImage(self):
-        global beforeImage, afterImage, blankImage
-        objectDetect.DetectObjectFromImage("Resources/testcase5/")
-
-    def FindCornerFromImage(self):
-        FindCorner.FindCornerFromImage("Resources/testcase7/")
+        self.assertEqual(cv2.__version__, Setting.DefineManager.USE_OPENCV_VERSION, "Wrong Version Installed")
 
 testSuite = unittest.TestSuite()
 testSuite.addTest(ProjectSettingTest('TestVersionOfOpenCV'))
-testSuite.addTest(ProjectCalculateLength('MakeBlankImage'))
-testSuite.addTest(ProjectCalculateLength('BeforeImageLoadTest'))
-testSuite.addTest(ProjectCalculateLength('AfterImageLoadTest'))
-testSuite.addTest(ProjectCalculateLength('ObjectDetectFromImage'))
-testSuite.addTest(ProjectCalculateLength('FindCornerFromImage'))
-#testSuite.addTest(ProjectCalculateLength('TestingDetecting'))
 unittest.TextTestRunner(verbosity=2).run(testSuite)
+
+objectDetect.DetectObjectFromImage(Setting.DefineManager.OBJECT_DETECT_TESTCASE_PATH)
+FindCorner.FindCornerFromImage(Setting.DefineManager.FIND_CORNER_TESTCASE_PATH)
