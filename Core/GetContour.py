c47b16a9cfded61f583ed48d7059324bcf2ac8a6
sunjbs <sunjbs98@gmail.com>
2017-04-19 21:32:44 +0900

    contour
    contour

---
+++ Core/GetContour.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import Utils.CustomOpenCV as ccv
+import Setting.DefineManager
+
+def GetContour(Image):
+    contourImage = np.ndarray(Image.shape)
+    ImageEdgesDetected = cv2.Canny(Image, Setting.DefineManager.CANNY_MINIMUM_THRESHOLD, Setting.DefineManager.CANNY_MAXIMUM_THRESHOLD)
+    _, contours, hierarchy = cv2.findContours(ImageEdgesDetected, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    ccv.ShowImagesWithName([ImageEdgesDetected])
+    maxContoursArea = 0
+    calculatedContourArea = 0
+    maxContoursIndex = []
+    for contoursIndex in contours:
+        calculatedContourArea = cv2.contourArea(contoursIndex)
+        if maxContoursArea < calculatedContourArea:
+            maxContoursArea = calculatedContourArea
+            maxContoursIndex = contoursIndex
+
+    cv2.drawContours(contourImage, contours, -1, 255, 3)
+    return contours, contourImage
--- Core/ObjectDetect.py
+++ Core/ObjectDetect.py
@@ -9,6 +9,7 @@ import ShapeDetectAndFindCorner
 import Utils.CustomOpenCV
 import Utils.LogManager
 
+import GetContour
 
 def GrayImage(before,after):
     #before :  Resources/testcase5/before.JPG
@@ -101,8 +102,7 @@ def GrayImage(before,after):
     differenceThresh = cv2.absdiff(beforeThresh, afterThresh)
     differenceThresh[differenceThresh > Setting.DefineManager.EACH_IMAGE_DIFFERENCE_THRESHOLD] = Setting.DefineManager.SET_IMAGE_WHITE_COLOR
 
-    Utils.CustomOpenCV.ShowImagesWithName([differenceMorph, differenceThresh], ['Morph','Thresh'])
-    #Show([resizeBefore, resizeAfter, differenceMorph], ['before','after','difference'])
+    #Utils.CustomOpenCV.ShowImagesWithName([differenceMorph, differenceThresh], ['Morph','Thresh'])
 
     for count in contours:
         approx = cv2.approxPolyDP(count, 0.1 * cv2.arcLength(count, True), True)
@@ -118,6 +118,9 @@ def GrayImage(before,after):
             for i in approx:
                 x, y = i.ravel()
                 cv2.circle(lineImage, (x, y), 1, (0, 255, 0), -1)
+
+    contour, contourImage = GetContour.GetContour(differenceMorph)
+    Utils.CustomOpenCV.ShowImagesWithName([differenceMorph,contourImage])
 
     return  resizeBefore, resizeAfter, differenceMorph, differenceThresh
 
