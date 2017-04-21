d7495602a15a85ec61c72ecc5b0304e7e722641d
Dinsslug <dinsslug@gmail.com>
2017-04-21 21:52:15 +0900

    배경 판넬의 외곽점 4개 찾기 (미완성)
    형광색을 색 인식하여 찾음
    Contours 4구역 각각에 대한 중간점 찾는 작업하면 완료

--- Core/ColorDetect.py
+++ Core/ColorDetect.py
@@ -3,22 +3,17 @@
 import cv2
 import numpy as np
 
-def ColorDetectFromImage(arg1):
-    origin_image = cv2.imread(arg1)
+def ColorDetectFromImage(image, lower_hsv, upper_hsv):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(origin_image, cv2.COLOR_BGR2HSV)
+    lower = np.array(lower_hsv, dtype=np.uint8)
+    upper = np.array(upper_hsv, dtype=np.uint8)
 
-    # define range of blue color in HSV
-    lower_blue = np.array([  0,  0,100], dtype=np.uint8)
-    upper_blue = np.array([255,255,255], dtype=np.uint8)
-
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower, upper)
 
     # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(origin_image, origin_image, mask=mask)
+    res = cv2.bitwise_and(image, image, mask=mask)
 
     #cv2.imwrite(arg2,  mask)
 
-    return mask
+    return res
